205822955a01f308fb916a1a62aa6a884c10e488
Matheus Vieira Costa <86197845+MatheusVieira9@users.noreply.github.com>
2024-08-06 17:42:06 -0300

    Commit project IOT
    Image and code.

---
+++ Code/sizzling_gaaris_bojo1.ino
@@ -0,0 +1,31 @@
+
+int pinTemp = A1;
+int pinCooler = 11;
+int pinAlert = 12;
+
+int temp = 0;
+
+
+void setup()
+{
+  pinMode(pinTemp, INPUT);
+  pinMode(pinCooler, OUTPUT);
+  pinMode(pinAlert, OUTPUT);
+}
+
+void loop()
+{
+  temp = (-40 + 0.488155 * (analogRead(pinTemp) - 20));
+  if (temp >= 30) {
+    digitalWrite(pinCooler, HIGH);
+    if (temp > 50) {
+      digitalWrite(pinAlert, HIGH);
+    } else {
+      digitalWrite(pinAlert, LOW);
+    }
+  } else {
+    digitalWrite(pinCooler, LOW);
+    digitalWrite(pinAlert, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
